3978f12ad9a5f92e36b312c77ea6fc7178c0c8e7
Nicolas Lamirault <nlamirault@gmail.com>
2009-01-16 17:54:51 +0100

    Update code coverage tool

--- misc/lastfm-cover.lisp
+++ misc/lastfm-cover.lisp
@@ -7,7 +7,7 @@
 ;;;; Purpose:       cl-lastfm code coverage.
 ;;;; Programmer:    Nicolas Lamirault <nlamirault@gmail.com>
 ;;;;
-;;;; This file, part of cl-lastfm, is Copyright (c) 2008 by Nicolas Lamirault
+;;;; This file, part of cl-lastfm, is Copyright (c) 2009 by Nicolas Lamirault
 ;;;;
 ;;;; cl-lastfm users are granted the rights to distribute and use this software
 ;;;; as governed by the terms of the MIT License :
@@ -15,13 +15,19 @@
 ;;;;
 ;;;; *************************************************************************
 
+(require :asdf)
 (require :sb-cover)
-     
+
 (declaim (optimize sb-cover:store-coverage-data))
 
 (asdf:oos 'asdf:load-op :cl-lastfm-test)    
+
 (cl-lastfm-test:run-cl-lastfm-test)
-(sb-cover:report "/tmp/cl-lastfm-coverage/")
+
+(let* ((path (namestring
+              (asdf:component-relative-pathname (asdf:find-system :cl-lastfm))))
+       (dir (concatenate 'string path "/www/coverage/")))
+  (ensure-directories-exist dir)
+  (sb-cover:report dir))
      
 (declaim (optimize (sb-cover:store-coverage-data 0)))
-
