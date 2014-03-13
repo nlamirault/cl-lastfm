5b9bfcdfe992dbc653b8f684a72bd1efcb5dbd89
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-13 18:08:05 +0100

    Update CI tool

--- ci/cl-lastfm-ci.sh
+++ ci/cl-lastfm-ci.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-# Cletris continuous integration tool
+# cl-lastfm continuous integration tool
 #
 
-set -e
-set -x
-export PROJECT=cl-lastfm
-export TEST_ENV=$PWD/.clenv
+SCRIPT_HOME=`dirname $0`
+PROJECT_HOME=$(dirname $SCRIPT_HOME)
+PROJECT="cl-lastfm"
+TEST_ENV=$PROJECT_HOME/.clenv
+QUICKLISP_FILE="http://beta.quicklisp.org/quicklisp.lisp"
 
 cleanup() {
     rm -fr $TEST_ENV
@@ -16,7 +17,7 @@ cleanup() {
 init() {
     cp ci/init.lisp $TEST_ENV
     cd $TEST_ENV
-    wget -q http://beta.quicklisp.org/quicklisp.lisp -O quicklisp.lisp
+    wget -q $QUICKLISP_FILE -O quicklisp.lisp
     sbcl --script init.lisp
     ln -s $PWD/.. ./.quicklisp/local-projects/$PROJECT
     cd ..
--- test/users.lisp
+++ test/users.lisp
@@ -7,8 +7,6 @@
 
 (defparameter *api-key* (sb-posix:getenv "CL-LASTFM-API-KEY"))
 
-(format t "API Key: ~A" (sb-posix:getenv "CL-LASTFM-API-KEY"))
-
 (define-test cant-get-user-events-without-api-key
   (handler-case
       (user-get-events "" "rj")
@@ -19,12 +17,21 @@
 
 (define-test can-get-user-events-with-name
   (format t "API Key: ~A" (sb-posix:getenv "CL-LASTFM-API-KEY"))
-  (let ((response
-         (user-get-events *api-key*
-			  "rj")))
+  (let ((response (user-get-events *api-key* "rj")))
     (assert-true (cl-ppcre:scan "<lfm status=\"ok\">" response))
     (assert-true (cl-ppcre:scan "<events " response))
     (let ((sum 0))
       (cl-ppcre:do-matches (s e "<user>" response)
         (incf sum))
       (assert-true (>= sum 0)))))
+
+(define-test test-event-get-info
+  (let ((response
+         (cl-lastfm:event-get-info *api-key* "328799")))
+    (assert-true (cl-ppcre:scan "<lfm status=\"ok\">" response))
+    (assert-true (cl-ppcre:scan "<event " response))
+    (assert-true (cl-ppcre:scan "<id>328799</id>" response))
+    (assert-true (cl-ppcre:scan "<artist>Philip Glass</artist>" response))
+    (assert-true (cl-ppcre:scan
+                  "<artist>Orchestra and Chorus of Erfurt Theatre</artist>"
+                  response))))
