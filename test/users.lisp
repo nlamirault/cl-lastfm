b7b7fdb65af5594773d1ade7610ebfd4aecce7d5
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-13 17:43:02 +0100

    Add API KEY

--- test/users.lisp
+++ test/users.lisp
@@ -4,6 +4,10 @@
 
 (in-package #:cl-lastfm-test)
 
+
+(defparameter *api-key* (sb-posix:getenv "CL-LASTFM-API-KEY"))
+
+
 (define-test cant-get-user-events-without-api-key
   (handler-case
       (cl-lastfm:user-get-events nil "rj")
@@ -12,14 +16,13 @@
       (assert-equal "10" (request-error-code-of condition)))))
 
 
-;; FIXME: find API Key
-;; (define-test can-get-user-events-with-name
-;;   (let ((response
-;;          (user-get-events "b25b959554ed76058ac220b7b2e0a026"
-;; 			  "rj")))
-;;     (assert-true (cl-ppcre:scan "<lfm status=\"ok\">" response))
-;;     (assert-true (cl-ppcre:scan "<events user=" response))
-;;     (let ((sum 0))
-;;       (cl-ppcre:do-matches (s e "<user>" response)
-;;         (incf sum))
-;;       (assert-true (>= sum 0)))))
+(define-test can-get-user-events-with-name
+  (let ((response
+         (user-get-events *api-key*
+			  "rj")))
+    (assert-true (cl-ppcre:scan "<lfm status=\"ok\">" response))
+    (assert-true (cl-ppcre:scan "<events " response))
+    (let ((sum 0))
+      (cl-ppcre:do-matches (s e "<user>" response)
+        (incf sum))
+      (assert-true (>= sum 0)))))
