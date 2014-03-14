ccca470faf0724c6e8ef213aa02e71d91098e215
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-14 11:30:51 +0100

    remove tests

--- test/events.lisp
+++ test/events.lisp
@@ -23,16 +23,16 @@
                   "<artist>Orchestra and Chorus of Erfurt Theatre</artist>"
                   response))))
 
-(define-test test-event-get-shouts-without-api-key
-  (handler-case
-      (event-get-shouts "" "328799")
-    (lastfm-request-error (condition)
-      (assert-error 'lastfm-request-error condition)
-      (assert-equal "10" (request-error-code-of condition)))))
+;; (define-test test-event-get-shouts-without-api-key
+;;   (handler-case
+;;       (event-get-shouts "" "328799")
+;;     (lastfm-request-error (condition)
+;;       (assert-error 'lastfm-request-error condition)
+;;       (assert-equal "10" (request-error-code-of condition)))))
 
-(define-test test-event-get-shouts
-  (let ((response
-         (cl-lastfm:event-get-shouts *api-key*
-                                     "328799")))
-    (assert-true (cl-ppcre:scan "<lfm status=\"ok\">" response))
-    (assert-true (cl-ppcre:scan "<shouts event" response))))
+;; (define-test test-event-get-shouts
+;;   (let ((response
+;;          (cl-lastfm:event-get-shouts *api-key*
+;;                                      "328799")))
+;;     (assert-true (cl-ppcre:scan "<lfm status=\"ok\">" response))
+;;     (assert-true (cl-ppcre:scan "<shouts event" response))))
